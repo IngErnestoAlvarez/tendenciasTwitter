c5d7918f61ea106a5cd04ae7a190f98d709068c1
Caliz Blanco Alejo <50003651+Merok23@users.noreply.github.com>
2019-06-11 11:45:00 -0300

    Agregué un lector de tweets (pendiente ver el funcionamiento)
    El lector crea una lista con todos los n (cantidad de lineas) tweets y utiliza tambien el strutil (puse el tuyo porque no confio en el mio).

--- procesarTweets.c
+++ procesarTweets.c
@@ -2,10 +2,13 @@
 #include <string.h>
 #include <stdbool.h>
 #include "minsketch.h"
+#include "strutil.h"
+#include "lista.h"
 
 /* *****************************************************************
  *                    FUNCIONES AUXILIARES
  * *****************************************************************/
+
 bool validar_parametros(int argc,char* argv[]){
 	if(argc != 3){
 		fprintf(stderr, "Error: La cantidad de parametros no es valida.");
@@ -15,17 +18,50 @@ bool validar_parametros(int argc,char* argv[]){
 		fprintf(stderr, "Error: El numero de lineas a leer no es valido.");
 		return false;
 	}
-	if(atoi(argv[2])<= 0){
+	if(atoi(argv[2]) <= 0){
 		fprintf(stderr, "Error: El numero de tendencias que pides no es valido");
 		return false;
 	}
 	return true;
 }
+
+
+/* Guarda tweets en una lista
+ * Pre: Cantidad de lineas es un numero positivo.
+ * Post: Devuelve una lista con todos los tweets leidos en 'cantidad_de lineas' lineas.
+ */
+
+lista_t* leer_tweets(size_t cantidad_de_lineas){
+	
+	lista_t* lista_de_tweets = lista_crear();
+	char* new_line[1];
+	new_line[0] = '\n';
+	size_t contador_de_lineas = 1;
+	size_t ultimo_new_line = 0;
+	size_t aux = 0; //Variable donde empieza el tweet
+
+	while(contador_de_lineas <= cantidad_de_lineas){
+		aux = ultimo_new_line;
+		ultimo_new_line = strcspn(stdin+ultimo_new_line, new_line); //Encuentra la posicion de '\n'
+		char* tweet = substr(stdin[aux], ultimo_new_line-aux); 
+		lista_insertar_ultimo(lista_de_tweets,tweet);
+		contador_de_lineas++;
+	}
+
+	return lista_de_tweets;
+}
+
+
+
+
 /* *****************************************************************
  *                      FUNCION PRINCIPAL
  * *****************************************************************/
 int main(int argc, char *argv[]){
 	if(!validar_parametros(argc, argv)) return -1;	
+	lista_de_tweets = leer_tweets(atoi(argv[1]));
 
+
+	lista_destruir(lista_de_tweets,free);
 	return 0;
 }
